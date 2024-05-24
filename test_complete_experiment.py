6f2edc707ca98e0be1c2d18e1bc9148e2747c0f3
Tim Schofield <timothyschofield@hotmail.com>
2024-05-24 14:00:12 +0100

    more experiments

--- test_complete_experiment.py
+++ test_complete_experiment.py
@@ -22,7 +22,7 @@ client = OpenAI(api_key=my_api_key)   # openai version 1.2.3
 
 try:
 
-    prompt = "Please read this text and return everything you can."
+    prompt = "Please read this text and return everything you can. Just retun what you see, do not make comments"
     
     url = "https://d2seqvvyy3b8p2.cloudfront.net/2ca62a26221a397d6942874b6ee7a225.jpg"
 
@@ -45,17 +45,23 @@ try:
         }  
     ]
 
-    ocr_output = client.chat.completions.create(model=model, messages=messages, stream=True)
+    # messages=[{"role": "user", "content": "Say this is a test"}]
+
+    ocr_output = client.chat.completions.create(model=model, messages=messages, stream=True, max_tokens=500)
     
-    print(ocr_output)
-
-    print("=====================================")
-
+    output_list = []
     for chunk in ocr_output:
-        print(chunk)
-        print(chunk.choices[0].delta.content)
-        print("****************")
-    
+        # print(chunk)
+        this_chunk = chunk.choices[0].delta.content
+        # print(this_chunk)
+        
+        output_list.append(str(this_chunk))
+        # print("****************")
+   
+    print("==============================================")
+    print("num chunks:", len(output_list))
+    print("".join(output_list))
+
     print("========================== STOP OUTPUT =============================")   
 
 except Exception as ex:
