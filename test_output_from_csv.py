bfc4cda2eed11fde93ea3a9f1e9e06786d917412
Tim Schofield <timothyschofield@hotmail.com>
2024-05-29 07:52:44 +0100

    Output folders

--- test_output_from_csv.py
+++ test_output_from_csv.py
@@ -1,4 +1,20 @@
 """
+File : test_output_from_csv.py
+
+Author: Tim Schofield
+Date: 28 May 2024
+
+print('scheme  :', url_parsed.scheme)
+print('netloc  :', url_parsed.netloc)
+print('path    :', url_parsed.path)
+print('params  :', url_parsed.params)
+print('query   :', url_parsed.query)
+print('fragment:', url_parsed.fragment)
+print('username:', url_parsed.username)
+print('password:', url_parsed.password)
+print('hostname:', url_parsed.hostname, '(netloc in lower case)')
+print('port    :', url_parsed.port)
+
 
 """
 from pathlib import Path 
@@ -9,31 +25,45 @@ import os
 from urllib.parse import urlparse
 
 """
+Required file hierachy
 
-uni-ucl-heb-0015052
-    uni-ucl-heb-0015052-001
+uni-ucl-heb-0015052                             <<<<< call this the section level
+    uni-ucl-heb-0015052-001                     <<<<< this is the item level
         uni-ucl-heb-0015052-001-0001L.xml
         uni-ucl-heb-0015052-001-0001R.xml   
         ...    
 """
 
-FILE_NAME = "Judaica_many_words_2024-05-27T17-58-00-6.csv"
-csv_folder = Path("output_gpt")
+FILE_NAME = "TIM METADATA - Proquest UCL - Judaica Batch 1 (C260_0003) - BENCHMARK.csv"
+csv_folder = Path("input_gpt")
 
-dest_folder = Path("xml_dest")
+dest_folder = Path("output_xml_folders")
 
 df = pd.read_csv(csv_folder / FILE_NAME)
 # print(df) 
 
-# https://lrfhec.maxcommunications.co.uk/LRF/JUDAICA/IMAGES/uni-ucl-heb-0015052/uni-ucl-heb-0015052-001/uni-ucl-heb-0015052-001-0001L.jpg
+
 for index, row in df.iterrows():
     
-    url = row["source"]
-    # print(f"{url}")
+    # https://lrfhec.maxcommunications.co.uk/LRF/JUDAICA/IMAGES/uni-ucl-heb-0015052/uni-ucl-heb-0015052-001/uni-ucl-heb-0015052-001-0001L.jpg
+    url = row["Image url link"]
+   
+    if url == "none": 
+        continue
+
+    print(f"{url}")
     
-    url_parsed = urlparse(url)
-    file_name = os.path.basename(url_parsed.path)
-    print(file_name)
+    url_list = url.split("/")
+    
+    # print(f"{url_list}")
+    
+    filename = url_list[-1]
+    item_name = url_list[-2]
+    section_name =  url_list[-3]
+    
+    print(f"{section_name=}\n{item_name=}\n{filename=}\n")
+
+    
     
     exit()
 
